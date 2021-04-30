7968394124de076e010348f857a04903a64b6b07
Dawn C. DeMeo <Dawn.DeMeo@gmail.com>
2021-04-30 16:09:14 -0400

    Initial commit

---
+++ 02_04/start/test/ScrollToElement.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ScrollToElement {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/dawndemeo/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/scroll");
+
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(name);
+        name.sendKeys("Dawn DeMeo");
+
+        WebElement date = driver.findElement(By.id("date"));
+        date.sendKeys("04/29/2010");
+
+        driver.quit();
+    }
+}
